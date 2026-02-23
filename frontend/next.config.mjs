b4684ccacfbed5fb6d82790cc12c86d8bf758dc1
Aastha Rao <BL0155@bliss.local>
2026-02-23 11:16:58 +0530

    changes in nextconfig for allwing cross origin

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    console.log(
+      "NEXT_PUBLIC_BACKEND in next config:",
+      process.env.NEXT_PUBLIC_BACKEND,
+    );
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_BACKEND}/api/:path*`,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
